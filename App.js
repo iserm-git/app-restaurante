da9e1cf3e7e4c0bb50c0983160a7061bc0f4ed64
Edgar Rojas <redgarm@gmail.com>
2022-05-13 18:07:06 -0500

    APP Restaurante - Sesion 2

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import MenuView from "./views/menu";
+import PedidoView from "./views/pedido";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={"Menu"}>
+        <Stack.Screen
+          name="Menu"
+          component={MenuView}
+          options={{ title: "Menu Section" }}
+        />
+        <Stack.Screen
+          name="Pedido"
+          component={PedidoView}
+          options={{ title: "Menu Section" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
